8b4b421594f2278161e4ef9ad1e5bc41553fb21c
Jose Cisneros <jmc@asu.edu>
2019-08-11 20:12:31 -0700

    init commit

---
+++ Linq/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Linq
+{
+    public class  Params: IEnumerable<int>
+    {
+        private int a, b, c;
+
+        public Params(int a, int b, int c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+        
+        public IEnumerator<int> GetEnumerator()
+        {
+            yield return a;
+            yield return b;
+            yield return c;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var p = new Params(1, 2, 3);
+            foreach (var x in p)
+            {
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
